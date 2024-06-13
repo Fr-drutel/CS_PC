d55bd4b999fc52a10c0a647c81a6daf34811f7f8
François-Régis <francois-regis.drutel@cpe.fr>
2024-06-13 16:12:01 +0200

    estimation_pi pixelo pixel

--- TPFINAL/estimation_pi.py
+++ TPFINAL/estimation_pi.py
@@ -1,7 +1,28 @@
-import random, time, multiprocessing as mp
+"""
+Programme ayant pour objectif de comparer l'efficacité d'un calcul de l'estimation de Pi 
+par la méthode de Monte-Carlo, en utilisant une approche mono-processus et une approche 
+multi-processus. Le programme calcule le nombre de points tombant dans un cercle unité 
+et estime la valeur de Pi en fonction du ratio de ces points par rapport au nombre total 
+d'itérations.
+Réalisé par François Régis Drutel et Paul Dumont le 13/06/2024
+À faire : Rien.
+"""
+
+import random
+import time
+import multiprocessing as mp
 
 # Calculer le nombre de hits dans un cercle unitaire (utilisé par les différentes méthodes)
 def frequence_de_hits_pour_n_essais(nb_iteration):
+    """
+    Calcule le nombre de points qui tombent dans le cercle unité.
+
+    Arguments d'entrée:
+        nb_iteration (integer): Nombre d'itérations pour l'estimation.
+
+    Arguments de sortie:
+        integer: Nombre de hits dans le cercle unité.
+    """
     count = 0
     for i in range(nb_iteration):
         x = random.random()
@@ -9,14 +30,20 @@ def frequence_de_hits_pour_n_essais(nb_iteration):
         if x * x + y * y <= 1: count += 1
     return count
 
-def creation_queue(nb_iteration, queue):
+def tache_processus(nb_iteration, queue):
+    """
+    Fonction de travail pour chaque processus.
+    
+    Args:
+        nb_iteration (integer): Nombre d'itérations pour l'estimation.
+        queue: queue pour stocker les résultats.
+    """
     count = frequence_de_hits_pour_n_essais(nb_iteration)
     queue.put(count)
 
-if __name__ == "__main__":
-   
-    nb_total_iteration = 10000000  # Nombre d’essai pour l’estimation
 
+if __name__ == "__main__":
+    nb_total_iteration = 10000000  # Nombre d’essai pour l’estimation
 
     # _________________________Version Mono-process____________________________#
 
@@ -34,7 +61,7 @@ if __name__ == "__main__":
 
     nb_process = 7  # Nombre de processus à utiliser
     nb_iterations_par_process = nb_total_iteration // nb_process
-    process = []
+    liste_process = []
     queue = mp.Queue()
     nb_hits = 0
 
@@ -42,15 +69,15 @@ if __name__ == "__main__":
     
     for i in range(nb_process):
         # On crée un process 
-        p = mp.Process(target=creation_queue, args=(nb_iterations_par_process, queue))
+        process = mp.Process(target=tache_processus, args=(nb_iterations_par_process, queue))
         # On le met dans la liste
-        process.append(p)
+        liste_process.append(process)
         # On le démarre 
-        p.start()
+        process.start()
 
     # Quand on a tous les process dans la liste ils join tous en même temps piu o meno     
-    for p in process:
-        p.join()
+    for process in liste_process:
+        process.join()
     
     # On récupére les résultats tant que la queue n'est pas vide, on utilise 'not'
     while not queue.empty():
